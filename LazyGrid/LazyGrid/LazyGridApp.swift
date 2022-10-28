d5269abe8d62c4a6622a463b1b2d17f369fb2306
Elbin <dh3183@naver.com>
2022-10-28 22:10:06 +0900

    LazyGrid

---
+++ LazyGrid/LazyGrid/LazyGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridApp.swift
+//  LazyGrid
+//
+//  Created by 한빈 on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
