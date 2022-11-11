0c995fc64aae0c28c4d5496c019fe03b366ac71c
Elbin <dh3183@naver.com>
2022-11-11 16:34:46 +0900

    .gitignore 생성

---
+++ Projects/KaKaoLogin/KaKaoLogin/KaKaoLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaKaoLoginApp.swift
+//  KaKaoLogin
+//
+//  Created by 한빈 on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct KaKaoLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
