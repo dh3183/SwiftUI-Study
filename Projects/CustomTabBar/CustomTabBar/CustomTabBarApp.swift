79bccadd9acc6fa380dcc3fe17fd5ea6711879b3
Elbin <dh3183@naver.com>
2022-11-11 23:31:29 +0900

    ➕[add]: Create Custom Tabbar Project

---
+++ Projects/CustomTabBar/CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by 한빈 on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
