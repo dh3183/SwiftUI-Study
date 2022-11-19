fbea565afcc780746a56ce314d410f8d7c8af468
Elbin <dh3183@naver.com>
2022-11-19 10:14:50 +0900

    ➕[add]: Create TabView Project

---
+++ Projects/TabV/TabV/TabVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabVApp.swift
+//  TabV
+//
+//  Created by 한빈 on 2022/11/19.
+//
+
+import SwiftUI
+
+@main
+struct TabVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
