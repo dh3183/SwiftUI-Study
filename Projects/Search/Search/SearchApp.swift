d8e83d8128ef2703dfb4f4eb6156300ec0a8089d
Elbin <dh3183@naver.com>
2022-11-21 04:59:06 +0900

    [add]: Create Searchbar Project

---
+++ Projects/Search/Search/SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchApp.swift
+//  Search
+//
+//  Created by 한빈 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
