4edd57e925f17a7423c82f9b4f78855c1e0238dd
Elbin <dh3183@naver.com>
2022-11-21 09:57:46 +0900

    [add]: Create Project
    modal Xcode project

---
+++ Projects/modalpra/modalpra/modalpraApp.swift
@@ -0,0 +1,17 @@
+//
+//  modalpraApp.swift
+//  modalpra
+//
+//  Created by 한빈 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct modalpraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
