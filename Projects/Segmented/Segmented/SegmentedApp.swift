e0ce79179383264004b8313d3ce7352ce8cd10af
Elbin <dh3183@naver.com>
2022-12-03 02:22:44 +0900

    ➕[add]: Create Project
    Segmented Project

---
+++ Projects/Segmented/Segmented/SegmentedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedApp.swift
+//  Segmented
+//
+//  Created by 한빈 on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
