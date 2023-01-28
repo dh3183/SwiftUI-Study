e70df41d660117235bef0e738361e4b80e08b61b
Elbin <dh3183@naver.com>
2023-01-28 20:35:00 +0900

    ➕[add]: Create Xcode Project
    Creating an Xcode Project for View Builder Practice

---
+++ Projects/ViewBuilder/ViewBuilder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ViewBuilder
+//
+//  Created by 한빈 on 2023/01/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/ViewBuilder/ViewBuilder/ViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderApp.swift
+//  ViewBuilder
+//
+//  Created by 한빈 on 2023/01/28.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
