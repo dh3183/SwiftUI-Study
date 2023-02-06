7df3270100191cc58b4b7b4e19468bea54fcdc59
Elbin <dh3183@naver.com>
2023-02-07 03:37:01 +0900

    ➕[add]: Create Gestures Xcode Project

---
+++ Projects/Gestures/Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by 한빈 on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
